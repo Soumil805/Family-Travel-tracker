a94ccfc41a9b72e5f6bcd46a0bf56522c5e6b520
Soumil Singh <singhsoumil805@gmail.com>
2024-12-07 15:13:05 +0530

    Fix Client is not defined issue

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 
+
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -10,8 +11,7 @@ const app = express();
 const port = 3000;
 
 
-
-const db = new Client({
+const db = new pg.Client({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
